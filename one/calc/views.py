3fa763525d95a82768b72f911d2fb4bf1dbad9e9
Sameer Kongath <info@zoomixinfo.in>
2021-06-10 12:24:34 +0530

    Add two number with post

--- one/calc/views.py
+++ one/calc/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html',{'name':'sameer'})
 def add(request):
-    val1=int(request.GET['num1'])
-    val2=int(request.GET['num2'])
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
     result = val1+val2
     return render(request,'result.html',{'result':result})
